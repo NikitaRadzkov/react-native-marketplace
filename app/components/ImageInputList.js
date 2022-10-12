69b4604d1c11092a4044390379988f7acefbb13e
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-12 13:12:35 +0300

    Add scroll to ImageInputList

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,19 +1,29 @@
-import { StyleSheet, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import React, { useRef } from 'react'
 import ImageInput from './ImageInput'
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef()
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput
-            imageUri={uri}
-            onChangeImage={() => onRemoveImage(uri)}
-        />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+            />
+            </View>
+          ))}
+            <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
         </View>
-      ))}
-        <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+      </ScrollView>
     </View>
   )
 }
