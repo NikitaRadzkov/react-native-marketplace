080b3f53f6de1f2d0057a66512da20632343e460
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-27 15:06:25 +0300

    Add FeedNavigator and refactor code

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import ListingsScreen from "../screens/ListingsScreen";
 import ListingsEditScreen from "../screens/ListingsEditScreen";
 import AccountScreen from "../screens/AccountScreen";
+import FeedNavigator from "./FeedNavigator";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name='Listings' component={ListingsScreen} />
+    <Tab.Screen name='Feed' component={FeedNavigator} />
     <Tab.Screen name='ListingsEdit' component={ListingsEditScreen} />
     <Tab.Screen name='Account' component={AccountScreen} />
   </Tab.Navigator>
